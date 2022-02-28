c368ae6bc9a8ce760a312bf113cca9a648561026
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-02-27 20:24:58 -0500

    s07-110 Moving the Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent },
   {
     path: 'training',
-    loadChildren: () => import('./training/training.module').then(m => m.TrainingModule)
+    loadChildren: () => import('./training/training.module').then(m => m.TrainingModule),
+    canLoad: [AuthGuard]
   }
 ];
 
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,26 +2,40 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
+  UrlSegment,
   UrlTree
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
 
-      if (this.authService.isAuth()) {
-        return true;
-      } else {
-        this.router.navigate(['/login']);
-        return false;
-      }
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]):
+    boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
 
 }
