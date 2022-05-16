5d0bb360b33089c8b1ccdd053345f24ca4064d7d
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-05-15 20:00:07 -0400

    s08-116 Working with Multiple Reducers & Actions

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,22 +1,19 @@
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector
+} from '@ngrx/store';
+import { UIActions } from './shared/ui.actions';
+
+import * as fromUi from './shared/ui.reducer';
+
 export interface State {
-  isLoading: boolean;
+  ui: fromUi.State
 }
 
-const initialState = {
-  isLoading: false
+export const reducers: ActionReducerMap<State, UIActions> = {
+  ui: fromUi.uiReducer
 };
 
-export function appReducer(state = initialState, action) {
-  switch(action.type) {
-    case 'START_LOADING':
-      return {
-        isLoading: true
-      };
-    case 'STOP_LOADING':
-      return {
-        isLoading: false
-      };
-    default:
-      return state;
-  }
-}
+export const getUiState = createFeatureSelector<fromUi.State>('ui');
+export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
