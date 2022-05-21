bbd9c7d1b6ac85a4c0ed4c3fedb97eae339653bf
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-05-21 16:14:28 -0400

    s08-119 Adding an Auth Reducer (and Actions)

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -6,14 +6,20 @@ import {
 import { UIActions } from './shared/ui.actions';
 
 import * as fromUi from './shared/ui.reducer';
+import * as fromAuth from './auth/auth.reducer';
 
 export interface State {
-  ui: fromUi.State
+  ui: fromUi.State;
+  auth: fromAuth.State;
 }
 
-export const reducers: ActionReducerMap<State, UIActions> = {
-  ui: fromUi.uiReducer
+export const reducers: ActionReducerMap<State, any> = {
+  ui: fromUi.uiReducer,
+  auth: fromAuth.authReducer
 };
 
 export const getUiState = createFeatureSelector<fromUi.State>('ui');
 export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
+
+export const getAuthState = createFeatureSelector<fromAuth.State>('auth');
+export const getIsAuth = createSelector(getAuthState, fromAuth.getIsAuth);
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { TrainingService } from '../training/training.service';
@@ -9,11 +8,10 @@ import { AuthData } from "./auth-data.model";
 import { UIService } from '../shared/ui.service';
 import * as fromRoot from '../app.reducer';
 import * as UI from '../shared/ui.actions';
+import * as Auth from '../auth/auth.actions';
 
 @Injectable()
 export class AuthService {
-  authChange = new Subject<boolean>();
-  private isAuthenticated = false;
 
   constructor(
     private router: Router,
@@ -26,14 +24,12 @@ export class AuthService {
   initAuthListener() {
     this.afAuth.authState.subscribe(user => {
       if (user) {
-        this.isAuthenticated = true;
-        this.authChange.next(true);
+        this.store.dispatch(new Auth.SetAuthenticated());
         this.router.navigate(['/training']);
       } else {
         this.trainingService.cancelSubscriptions();
-        this.authChange.next(false);
+        this.store.dispatch(new Auth.SetUnauthenticated());
         this.router.navigate(['/login']);
-        this.isAuthenticated = false;
       }
     });
   }
@@ -78,11 +74,5 @@ export class AuthService {
 
   logout() {
     this.afAuth.signOut();
-
   }
-
-  isAuth(): boolean {
-    return this.isAuthenticated;
-  }
-
 }
