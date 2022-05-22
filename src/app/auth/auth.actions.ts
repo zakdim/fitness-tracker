92d871868de84f68af2ffa9ef5632d4f83c5b9ba
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-05-22 13:38:47 -0400

    s08-121 Adding the Training Reducer and Actions with Payloads

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store';
 
 export const SET_AUTHENTICATED = '[Auth] Set Authenticated';
-export const SET_UNAUTHENTICATED = 'Auth] Set Unauthenticated';
+export const SET_UNAUTHENTICATED = '[Auth] Set Unauthenticated';
 
 export class SetAuthenticated implements Action {
   readonly type = SET_AUTHENTICATED;
